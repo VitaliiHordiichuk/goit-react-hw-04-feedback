18e34a2ef5e857dd4ec5c83d4e372ddc74c07fda
Vialii Hordiichuk <vgordiychuk5@gmail.com>
2022-10-26 20:40:32 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import css from './Notification/Notification.module.css';
-import PropTypes from 'prop-types';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions'
 import Section from './Section/Section'
 import Statistics from './Statistics/Statistics'
